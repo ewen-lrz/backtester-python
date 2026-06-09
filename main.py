f7537ed8e7ee727ccb678c588064c652305d05fb
ewen-lrz <ewen.lorenzi55@gmail.com>
2026-06-09 15:45:32 +0200

    Refactor: local CSV caching for faster execution and data privacy

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #from matplotlib import pyplot as plt
 import yfinance as yf
-#import pandas as pd
+import pandas as pd
 
 #Initialisation des varibles
 data = 0
 
-MU = yf.Ticker("MU")
+#Recuperer les données
+#MU = yf.Ticker("MU")
 
 #print(MU.history(start="2023-03-14", end="2026-03-14", interval="1wk"))
 
-data = MU.history(start="2023-03-14", end="2026-03-14", interval="1wk")
+#data = MU.history(start="2026-02-14", end="2026-03-14", interval="1wk")
 
-print(data.head(2))
+#data.to_csv("data/mu_data_1wk.csv")
+data = pd.read_csv("data/MU_data_1wk.csv")
+
+print(data.head(100))
