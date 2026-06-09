69460aa56b05fa370e587a7169c75a3787ec6428
ewen-lrz <ewen.lorenzi55@gmail.com>
2026-06-09 23:17:23 +0200

    Chore: set Date as DataFrame index for historical data alignment

--- main.py
+++ main.py
@@ -2,17 +2,18 @@
 import yfinance as yf
 import pandas as pd
 
-#Initialisation des varibles
+#Variable init
 data = 0
 
-#Recuperer les données
+#Define MU w/ MU stocks
 #MU = yf.Ticker("MU")
 
-#print(MU.history(start="2023-03-14", end="2026-03-14", interval="1wk"))
-
-#data = MU.history(start="2026-02-14", end="2026-03-14", interval="1wk")
+#Get data on a period of time w/ a time frame
+#data = MU.history(start="2022-03-14", end="2026-03-14", interval="1wk")
 
 #data.to_csv("data/mu_data_1wk.csv")
-data = pd.read_csv("data/MU_data_1wk.csv")
 
-print(data.head(100))
+#Def data w/ value in the csv, use index to define which column is the "main one" (index one)
+data = pd.read_csv("data/MU_data_1wk.csv", index_col=0)
+
+print(data.head(2))
